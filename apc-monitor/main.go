b4807210e5e4fbd34bc24d05da52e1d3ca888a11
iBug <git@ibugone.com>
2023-05-03 15:37:53 +0800

    apc-monitor: Switch datasource to SNMP

--- apc-monitor/main.go
+++ apc-monitor/main.go
@@ -1,25 +1,19 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"encoding/json"
 	"flag"
-	"io"
 	"log"
 	"os"
-	"strconv"
-	"strings"
 	"time"
 
+	"github.com/gosnmp/gosnmp"
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
-	"golang.org/x/crypto/ssh"
 )
 
 type Config struct {
 	Host     string `json:"host"`
-	Username string `json:"username"`
-	Password string `json:"password"`
 	InfluxDB struct {
 		Host     string `json:"host"`
 		Token    string `json:"token"`
@@ -50,63 +44,29 @@ func main() {
 	influxdb := influxdb2.NewClient(config.InfluxDB.Host, config.InfluxDB.Token)
 	writeAPI := influxdb.WriteAPIBlocking("", config.InfluxDB.Database)
 
-	sshConfig := &ssh.ClientConfig{
-		User:              config.Username,
-		Auth:              []ssh.AuthMethod{ssh.Password(config.Password)},
-		HostKeyCallback:   ssh.InsecureIgnoreHostKey(),
-		HostKeyAlgorithms: []string{"ssh-rsa"},
-		ClientVersion:     "SSH-2.0-OpenSSH", // wtf APC???
-	}
-
-	client, err := ssh.Dial("tcp", config.Host, sshConfig)
-	if err != nil {
-		panic(err)
-	}
-	defer client.Close()
-
-	session, err := client.NewSession()
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
-
-	stdinR, stdinW := io.Pipe()
-	session.Stdin = stdinR
-
-	stdoutR, stdoutW := io.Pipe()
-	session.Stdout = stdoutW
-
-	session.Stderr = io.Discard
-
-	if err := session.Shell(); err != nil {
+	snmp := new(gosnmp.GoSNMP)
+	*snmp = *gosnmp.Default
+	snmp.Target = config.Host
+	if err := snmp.Connect(); err != nil {
 		panic(err)
 	}
 
-	go func() {
-		for t := range time.NewTicker(1 * time.Second).C {
-			_ = t
-			stdinW.Write([]byte("phReading all current\n"))
+	oid := "1.3.6.1.4.1.318.1.1.12.2.3.1.1.2.1"
+
+	for t := range time.NewTicker(1 * time.Second).C {
+		_ = t
+		result, err := snmp.Get([]string{oid})
+		if err != nil {
+			log.Println(err)
+			continue
 		}
-	}()
-
-	scanner := bufio.NewScanner(stdoutR)
-	for scanner.Scan() {
-		t := scanner.Text()
-		f := strings.Fields(t)
-		if len(f) >= 2 && f[0] == "1:" {
-			value, err := strconv.ParseFloat(f[1], 64)
-			if err != nil {
-				log.Println(err)
-				continue
-			}
-			log.Println(value)
-			p := influxdb2.NewPointWithMeasurement("current").
-				AddField("_value", value).
-				SetTime(time.Now())
-			err = writeAPI.WritePoint(context.Background(), p)
-			if err != nil {
-				log.Printf("WritePoint: %v", err)
-			}
+		value := float64(result.Variables[0].Value.(uint)) / 10.0
+		p := influxdb2.NewPointWithMeasurement("current").
+			AddField("_value", value).
+			SetTime(time.Now())
+		err = writeAPI.WritePoint(context.Background(), p)
+		if err != nil {
+			log.Printf("WritePoint: %v", err)
 		}
 	}
 }
