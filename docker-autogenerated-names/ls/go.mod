fd4354e8e3edcc32169d2aefc8e08129dcd2459c
iBug <git@ibugone.com>
2023-05-23 23:39:40 +0800

    d-a-n: Reorganize files

--- docker-autogenerated-names/ls/go.mod
+++
@@ -1,3 +0,0 @@
-module docker-ps
-
-go 1.20
--- docker-autogenerated-names/main.go
+++ docker-autogenerated-names/main.go
@@ -1,22 +1,57 @@
 package main
 
 import (
-	"bufio"
+	"context"
 	"fmt"
-	"os"
 	"regexp"
 	"strings"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/ryanuber/columnize"
 )
 
+var (
+	ls = strings.Join(left[:], "|")
+	rs = strings.Join(right[:], "|")
+	re = regexp.MustCompile(fmt.Sprintf("\\b(%s)_(%s)\\b", ls, rs))
+)
+
+func isAutoName(s string) bool {
+	return re.FindString(s) != ""
+}
+
 func main() {
-	ls := strings.Join(left[:], "|")
-	rs := strings.Join(right[:], "|")
-	re := regexp.MustCompile(fmt.Sprintf("\\b(%s)_(%s)\\b", ls, rs))
-	s := bufio.NewScanner(os.Stdin)
-	for s.Scan() {
-		t := re.FindString(s.Text())
-		if t != "" {
-			fmt.Println(t)
-		}
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(
+		client.FromEnv,
+		client.WithAPIVersionNegotiation(),
+	)
+	if err != nil {
+		panic(err)
 	}
+
+	options := types.ContainerListOptions{
+		All: true,
+	}
+	containers, err := cli.ContainerList(ctx, options)
+	if err != nil {
+		panic(err)
+	}
+
+	lines := []string{"X | State | ID | Name | Image"}
+	for _, c := range containers {
+		name := strings.TrimPrefix(c.Names[0], "/")
+		line := []string{".",
+			c.State,
+			c.ID[:10],
+			name,
+			c.Image,
+		}
+		if isAutoName(name) {
+			line[0] = "X"
+		}
+		lines = append(lines, strings.Join(line, " | "))
+	}
+	fmt.Println(columnize.SimpleFormat(lines))
 }
